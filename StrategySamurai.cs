c5a06ce415f5ab280b7edd68a335582d8e5b9004
KhmylkoPavel <p.khmylko@hypervsn.com>
2021-04-13 22:41:37 +0300

    Add code

---
+++ StrategySamurai.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class StrategySamurai
+    {
+        static void Main(string[] args)
+        {
+            string currentMap = "Kyoto";
+            Warrior war = new Warrior();
+
+            if(currentMap == "Kyoto")
+            {
+                war.SetAttacker(new Samurai());
+            }
+            else
+            {
+                war.SetAttacker(new Berserk());
+            }
+
+            war.Attack();
+        }
+    }
+
+    public class Warrior
+    {
+        private IAttack _attacker;
+
+        public Warrior() { }
+
+        public Warrior(IAttack attacker) 
+        {
+            _attacker = attacker;
+        }
+
+        public void SetAttacker(IAttack attacker)
+        {
+            _attacker = attacker;
+        }
+
+        public void Attack()
+        {
+            if(_attacker != null)
+            {
+                _attacker.PerformAttack();
+            }
+        }
+    }
+
+    public interface IAttack
+    {
+        void PerformAttack();
+    }
+
+    class Berserk : IAttack
+    {
+        public void PerformAttack()
+        {
+            Console.WriteLine("Beau Beau Beau");
+        }
+    }
+
+    class Samurai : IAttack
+    {
+        public void PerformAttack()
+        {
+            Console.WriteLine("ボー ボー ボー");
+        }
+    }
+}
